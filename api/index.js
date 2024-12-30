127412333729987b21f5f6f6a8bc24a2dcbacbc1
Gargi Sukhatankar <gargi.p.sukhatankar@gmail.com>
2024-12-30 23:13:58 +0530

    updated

--- api/index.js
+++ api/index.js
@@ -1,11 +1,15 @@
 import express from "express";
+import cors from "cors";
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res)=>{
     res.json("test");
 })
 
-app.post("/api/transaction", (req, res)=>{
+app.post('api/transaction', (req, res)=>{
     res.json(req.body);
 })
 
