e27578c550a425698b0119283a8f8cca1930339e
Gargi Sukhatankar <gargi.p.sukhatankar@gmail.com>
2025-01-01 23:14:32 +0530

    updated

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import cors from "cors";
+import Transaction from "./models/Transaction.js"
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 const app = express();
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
@@ -10,6 +14,9 @@ app.get('/', (req, res)=>{
 })
 
 app.post('/transaction', (req, res)=>{
+    console.log(process.env.MONGO_URL);
+    // mongoose.connect('');
+    const { name, dateTime, description } = req.body;
     res.json(req.body);
 })
 
